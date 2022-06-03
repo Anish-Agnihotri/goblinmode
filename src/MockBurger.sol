3b1fa466738548ee0e2ec197172e7691e802b3ce
Anish Agnihotri <contact@anishagnihotri.com>
2022-06-02 20:53:57 -0400

    feat: MockBurger.sol

--- src/MockBurger.sol
+++ src/MockBurger.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/// ============ Imports ============
+
+import "solmate/tokens/ERC721.sol"; // Solmate: ERC721
+
 /// ============ Interfaces ============
 
 interface ExtendedERC721 {
@@ -12,7 +16,7 @@ interface ExtendedERC721 {
 
 /// @title MockBurger
 /// @notice Mocks hypothetical functionality of a McGoblinBurger contract
-contract MockBurger {
+contract MockBurger is ERC721("MockBurger", "MB") {
   // ============ Immutable storage ===========
 
   /// @dev goblintown NFT contract
@@ -26,12 +30,21 @@ contract MockBurger {
     GOBLIN_TOWN = ExtendedERC721(_GOBLIN_TOWN);
   }
 
+  // ============ Functions ============
+
   /// @notice Arbitrary claim function
+  /// @param tokenIds to claim
   function claim(uint256[] memory tokenIds) external {
     for (uint256 i = 0; i < tokenIds.length; i++) {
       // Require ownership of gobbler
       require(GOBLIN_TOWN.ownerOf(tokenIds[i]) == msg.sender, "oNlY foR GOblEn");
       // Mint new Mockburger NFT
+      _mint(msg.sender, tokenIds[i]);
     }
+  }
+
+  /// @notice Implement mock tokenURI
+  function tokenURI(uint256 id) public view override returns (string memory) {
+    return "";
   }
 }
